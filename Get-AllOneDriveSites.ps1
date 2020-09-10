f9d2bd2f30e7fb6d4955bb9da735a8004fde2403
gstires8 <67028131+gstires8@users.noreply.github.com>
2020-09-10 09:25:26 -0600

    Add files via upload

---
+++ Get-AllOneDriveSites.ps1
@@ -0,0 +1,28 @@
+﻿$AdminSiteURL="https://<TENANT-NAME>-admin.sharepoint.com"
+ 
+#Get Credentials to connect to the SharePoint Admin Center
+$Cred = Get-Credential
+ 
+#Connect to SharePoint Online Admin Center
+Connect-SPOService -Url $AdminSiteURL –credential $Cred
+ 
+#Get all Personal Site collections and export to a Text file
+$OneDriveSites = Get-SPOSite -Template "SPSPERS" -Limit ALL -includepersonalsite $True
+ 
+$Result=@()
+# Get storage quota of each site
+Foreach($Site in $OneDriveSites)
+{
+    $Result += New-Object PSObject -property @{
+    URL = $Site.URL
+    Owner= $Site.Owner
+    Size_inGB = $Site.StorageUsageCurrent/1024
+    #Size_inMB = $Site.StorageUsageCurrent
+    StorageQuota_inGB = $Site.StorageQuota/1024
+    }
+}
+ 
+$Result | Format-Table
+ 
+#Export the data to CSV
+$Result | Export-Csv "E:\Temp\GulfTechGroup\OneDrive-Details.csv" -NoTypeInformation
